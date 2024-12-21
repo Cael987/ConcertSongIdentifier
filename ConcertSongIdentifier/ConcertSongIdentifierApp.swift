7c725286fddb148211250d2a16a6cf418349307b
Cael987 <caelen@netsprout.com>
2024-12-21 09:34:11 -0800

    Initial Commit

---
+++ ConcertSongIdentifier/ConcertSongIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcertSongIdentifierApp.swift
+//  ConcertSongIdentifier
+//
+//  Created by Caelen Fry on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcertSongIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcertSongIdentifierTests/ConcertSongIdentifierTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConcertSongIdentifierTests.swift
+//  ConcertSongIdentifierTests
+//
+//  Created by Caelen Fry on 12/21/24.
+//
+
+import Testing
+@testable import ConcertSongIdentifier
+
+struct ConcertSongIdentifierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
